043e553390a36866024834c1d72617277892079e
Blair Harris <blairehh@gmail.com>
2022-08-13 17:43:37 +0200

    transpile empty file

--- src/main/java/org/bpc/transpile/java/TranspileFileToJavaClass.java
+++ src/main/java/org/bpc/transpile/java/TranspileFileToJavaClass.java
@@ -2,9 +2,18 @@ package org.bpc.transpile.java;
 
 import org.bpc.transpile.TranspileFile;
 
-public class TranspileFileToJavaClass {
-    public String toJavaClass(TranspileFile file) {
+import java.util.Base64;
 
-        return "";
+public class TranspileFileToJavaClass {
+
+    private final Base64.Encoder b64 = Base64.getEncoder();
+
+    public String toJavaClass(TranspileFile file) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("public static class _f_");
+        builder.append(b64.encodeToString(file.path().getBytes()));
+        builder.append(" {\n");
+        builder.append("}");
+        return builder.toString().trim();
     }
 }
--- src/test/java/org/bpc/transpile/java/TranspileFileToJavaClassTest.java
+++ src/test/java/org/bpc/transpile/java/TranspileFileToJavaClassTest.java
@@ -1,13 +1,29 @@
 package org.bpc.transpile.java;
 
+import org.bpc.transpile.TranspileFile;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TranspileFileToJavaClassTest {
 
+    TranspileFileToJavaClass transpile = new TranspileFileToJavaClass();
+
     @Test
     void testEmptyTranspileFile() {
+        TranspileFile file = new TranspileFile(
+            "./main.bp",
+            List.of()
+        );
 
+        String javaCode = transpile.toJavaClass(file);
+
+        assertThat(javaCode).isEqualTo("""
+            public static class _f_Li9tYWluLmJw {
+            }
+            """.trim());
     }
 }
