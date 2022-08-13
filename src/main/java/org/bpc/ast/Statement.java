08589cb8e57c3b1e1c25a19147a851114bc34b0a
Blair Harris <blairehh@gmail.com>
2022-08-13 17:31:11 +0200

    add todo

--- src/main/java/org/bpc/ast/Statement.java
+++ src/main/java/org/bpc/ast/Statement.java
@@ -4,6 +4,7 @@ import org.bpc.compile.Registry;
 
 import java.util.List;
 
+// TODO merge this with Expr?
 public interface Statement {
     List<Type> getTypesUsed();
     Statement canonicalizeStatement(Registry registry);
