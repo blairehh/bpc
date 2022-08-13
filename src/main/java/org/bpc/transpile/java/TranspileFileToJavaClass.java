fd3c3bd3530245fb71338665bf7848bfae662c9f
Blair Harris <blairehh@gmail.com>
2022-08-13 17:38:50 +0200

    sime renaming

---
+++ src/main/java/org/bpc/transpile/java/TranspileFileToJavaClass.java
@@ -0,0 +1,10 @@
+package org.bpc.transpile.java;
+
+import org.bpc.transpile.TranspileFile;
+
+public class TranspileFileToJavaClass {
+    public String toJavaClass(TranspileFile file) {
+
+        return "";
+    }
+}
--- src/test/java/org/bpc/transpile/java/TranspileFileToJavaClassTest.java
+++ src/test/java/org/bpc/transpile/java/TranspileFileToJavaClassTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CodeFileToJavaClassTest {
+class TranspileFileToJavaClassTest {
 
     @Test
     void testEmptyTranspileFile() {
