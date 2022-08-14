e0924c82df3c4e3e9c94dcb8be2de999f8a71d6d
Blair Harris <blairehh@gmail.com>
2022-08-14 16:54:21 +0200

    separete ProcedureCall and ProcedureExpr

---
+++ src/main/java/org/bpc/ast/ProcedureExpr.java
@@ -0,0 +1,34 @@
+package org.bpc.ast;
+
+import org.bpc.compile.Registry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public record ProcedureExpr(Identifier name, List<Expr> arguments) implements Expr, Assignable {
+
+    public ProcedureExpr(Identifier name) {
+        this(name,  new ArrayList<>());
+    }
+
+    @Override
+    public void assign(Expr expr) {
+        this.arguments.add(expr);
+    }
+
+    @Override
+    public List<Type> getTypesUsed() {
+        return this.arguments
+            .stream()
+            .flatMap((argument) -> argument.getTypesUsed().stream())
+            .toList();
+    }
+
+    @Override
+    public ProcedureExpr canonicalize(Registry registry) {
+        return new ProcedureExpr(
+            this.name,
+            this.arguments().stream().map((expr) -> expr.canonicalize(registry)).toList()
+        );
+    }
+}
